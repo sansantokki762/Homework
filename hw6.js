dfd7169d86491ce37410d993afc5eabd9a6d3264
nhatanh <nhatanh.abtx@gmail.com>
2026-02-07 23:16:51 +0900

    finally....

---
+++ hw6.js
@@ -0,0 +1,10 @@
+let sub = document.querySelector(".menu");
+
+function gay() {
+    if (sub.style.width == "0px") {
+        sub.style.width = "250px";
+    } 
+    else {
+        sub.style.width = "0px";
+    }
+}
